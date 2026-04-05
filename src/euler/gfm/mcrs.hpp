122860453d5374c371c0f690c40391706792c7bb
SegunOwoeye <olusegunowoeye@icloud.com>
2026-04-05 01:03:33 +0100

    Update mcrs.hpp

--- src/euler/gfm/mcrs.hpp
+++ src/euler/gfm/mcrs.hpp
@@ -2,11 +2,13 @@
 
 #include <algorithm>
 #include <cmath>
+#include <stdexcept>
 
-#include "src/euler/state.hpp"
 #include "src/euler/primitives.hpp"
 #include "src/euler/eos_params.hpp"
+#include "src/math/numerical_safety.hpp"
 
+// Completetly add in numerical saftey
 
 // [0] Mixed Riemann Solver Result
 struct MCRSResult1D {
@@ -17,9 +19,7 @@ struct MCRSResult1D {
 };
 
 
-// [1] Mixed-Component Riemann Solver in 1D Normal Direction
-    // This solver currently uses ideal-gas-type shock and rarefaction relations
-    // with possibly different gamma values on the left and right.
+// [1] Mixed-Component Riemann Solver (normal direction)
 class MCRS1D {
 public:
     // [1.1] Constructor
@@ -30,171 +30,260 @@ public:
         const EOSParams& paramsR,
         double tol = 1e-10,
         int max_iterations = 200
-    )
-    :
+    ):
         PL_S(PL),
         PR_S(PR),
         paramsL_S(paramsL),
         paramsR_S(paramsR),
-        tol_S(tol),
-        max_iterations_S(max_iterations)
+        tol_S(clamp_min(tol, 1e-14)),
+        max_iterations_S(std::max(max_iterations, 1))
     {
-        rhoL_S = std::max(PL_S.rho, tol_S);
-        rhoR_S = std::max(PR_S.rho, tol_S);
+        // enforce valid inputs
+        rhoL_S = require_positive(PL_S.rho, "MCRS: rhoL invalid", tol_S);
+        rhoR_S = require_positive(PR_S.rho, "MCRS: rhoR invalid", tol_S);
 
-        uL_S = PL_S.vel[0];
-        uR_S = PR_S.vel[0];
+        uL_S = require_finite(PL_S.vel[0], "MCRS: uL invalid");
+        uR_S = require_finite(PR_S.vel[0], "MCRS: uR invalid");
 
-        pL_S = std::max(PL_S.p, tol_S);
-        pR_S = std::max(PR_S.p, tol_S);
+        pL_S = require_positive(PL_S.p, "MCRS: pL invalid", tol_S);
+        pR_S = require_positive(PR_S.p, "MCRS: pR invalid", tol_S);
 
-        cL_S = std::sqrt(paramsL_S.gamma * pL_S / rhoL_S);
-        cR_S = std::sqrt(paramsR_S.gamma * pR_S / rhoR_S);
+        // sound speeds (ideal gas assumption)
+        cL_S = compute_c(rhoL_S, pL_S, paramsL_S);
+        cR_S = compute_c(rhoR_S, pR_S, paramsR_S);
     }
-
 
     // [1.2] Solve mixed-material Riemann problem
     inline MCRSResult1D solve() const
     {
-        const double newton_tol = std::max(tol_S, 1e-12);
-        const double p_floor = 1e-12;
-        const double rho_floor = 1e-12;
+        check_vacuum();
+
+        const double p_floor = tol_S;
+        const double rho_floor = tol_S;
 
         // [1.2.1] PVRS initial guess
         const double rho_bar = 0.5 * (rhoL_S + rhoR_S);
         const double a_bar = 0.5 * (cL_S + cR_S);
 
-        const double pPV = 0.5 * (pL_S + pR_S)
-            - 0.5 * (uR_S - uL_S) * rho_bar * a_bar;
+        double p_star = clamp_min(
+            0.5 * (pL_S + pR_S)
+            - 0.5 * (uR_S - uL_S) * rho_bar * a_bar,
+            p_floor
+        );
 
-        double p_star = std::max(pPV, p_floor);
+        // [1.2.2] Newton solve
+        bool converged = false;
 
-        // [1.2.2] Newton iteration
         for (int n = 0; n < max_iterations_S; ++n) {
-            const double F = fL(p_star) + fR(p_star) + (uR_S - uL_S);
 
-            if (std::abs(F) < newton_tol) {
+            const double FL  = fL(p_star);
+            const double FR  = fR(p_star);
+            const double dFL = dfL(p_star);
+            const double dFR = dfR(p_star);
+
+            const double F  = FL + FR + (uR_S - uL_S);
+            const double dF = dFL + dFR;
+
+            if (std::abs(F) < tol_S) {
+                converged = true;
                 break;
             }
 
-            const double dp = std::max(1e-6 * p_star, 1e-12);
-            const double F2 = fL(p_star + dp) + fR(p_star + dp) + (uR_S - uL_S);
-            const double dF = (F2 - F) / dp;
+            const double step = safe_div(F, dF, tol_S);
+            const double p_new = p_star - step;
 
-            if (std::abs(dF) < 1e-14) {
-                break;
-            }
+            const double p_safe = clamp_min(p_new, p_floor);
 
-            const double p_new = p_star - F / dF;
-            const double p_safe = std::max(p_new, p_floor);
-
-            const double denom = std::max(1.0, std::abs(p_star));
-            if (std::abs(p_safe - p_star) / denom < 10.0 * newton_tol) {
-                p_star = p_safe;
-                break;
-            }
+            const double rel = std::abs(p_safe - p_star) / std::max(1.0, std::abs(p_star));
 
             p_star = p_safe;
+
+            if (rel < 10.0 * tol_S) {
+                converged = true;
+                break;
+            }
         }
 
-        // [1.2.3] Star velocity
-        const double u_star =
-            0.5 * (uL_S + uR_S + fR(p_star) - fL(p_star));
-
-        // [1.2.4] Star densities
-        double rhoL_star;
-        double rhoR_star;
-
-        if (p_star > pL_S) {
-            const double gammaL = paramsL_S.gamma;
-
-            const double num =
-                (p_star / pL_S) + (gammaL - 1.0) / (gammaL + 1.0);
-
-            const double den =
-                ((gammaL - 1.0) / (gammaL + 1.0)) * (p_star / pL_S) + 1.0;
-
-            rhoL_star = rhoL_S * (num / den);
-        }
-        else {
-            rhoL_star = rhoL_S * std::pow(p_star / pL_S, 1.0 / paramsL_S.gamma);
+        if (!converged) {
+            p_star = clamp_min(0.5 * (pL_S + pR_S), p_floor);
         }
 
-        if (p_star > pR_S) {
-            const double gammaR = paramsR_S.gamma;
+        p_star = require_positive(p_star, "MCRS: p_star invalid", p_floor);
 
-            const double num =
-                (p_star / pR_S) + (gammaR - 1.0) / (gammaR + 1.0);
+        // [1.2.3] velocity
+        const double u_star = 0.5 * (uL_S + uR_S + fR(p_star) - fL(p_star));
 
-            const double den =
-                ((gammaR - 1.0) / (gammaR + 1.0)) * (p_star / pR_S) + 1.0;
+        // [1.2.4] densities
+        double rhoL_star = rho_star_L(p_star);
+        double rhoR_star = rho_star_R(p_star);
 
-            rhoR_star = rhoR_S * (num / den);
-        }
-        else {
-            rhoR_star = rhoR_S * std::pow(p_star / pR_S, 1.0 / paramsR_S.gamma);
-        }
+        rhoL_star = require_positive(rhoL_star, "MCRS: rhoL* invalid", rho_floor);
+        rhoR_star = require_positive(rhoR_star, "MCRS: rhoR* invalid", rho_floor);
 
-        rhoL_star = std::max(rhoL_star, rho_floor);
-        rhoR_star = std::max(rhoR_star, rho_floor);
-
-        return {p_star, u_star, rhoL_star, rhoR_star};
+        return {
+            p_star,
+            require_finite(u_star, "MCRS: u_star invalid"),
+            rhoL_star,
+            rhoR_star
+        };
     }
 
 private:
-    Primitive<1> PL_S;
-    Primitive<1> PR_S;
-
-    EOSParams paramsL_S;
-    EOSParams paramsR_S;
+    Primitive<1> PL_S, PR_S;
+    EOSParams paramsL_S, paramsR_S;
 
     double tol_S;
     int max_iterations_S;
 
-    double rhoL_S;
-    double rhoR_S;
-    double uL_S;
-    double uR_S;
-    double pL_S;
-    double pR_S;
-    double cL_S;
-    double cR_S;
+    double rhoL_S, rhoR_S;
+    double uL_S, uR_S;
+    double pL_S, pR_S;
+    double cL_S, cR_S;
 
 
-    // [1.3] Left wave function
+    // sound speed 
+    inline double compute_c(double rho, double p, const EOSParams& params) const
+    {
+        const double val = params.gamma * safe_div(p, rho, tol_S);
+        return require_positive(std::sqrt(val), "MCRS: sound speed invalid", tol_S);
+    }
+
+
+    // vacuum check 
+    inline void check_vacuum() const
+    {
+        const double crit =
+            (2.0 * cL_S / (paramsL_S.gamma - 1.0))
+          + (2.0 * cR_S / (paramsR_S.gamma - 1.0));
+
+        if ((uR_S - uL_S) >= crit) {
+            throw std::runtime_error("MCRS: vacuum state detected");
+        }
+    }
+
+
+    // wave functions 
     inline double fL(double p_star) const
     {
-        const double gammaL = paramsL_S.gamma;
+        const double p = clamp_min(p_star, tol_S);
+        const double g = paramsL_S.gamma;
 
-        if (p_star > pL_S) {
-            const double A = 2.0 / ((gammaL + 1.0) * rhoL_S);
-            const double B = ((gammaL - 1.0) / (gammaL + 1.0)) * pL_S;
+        if (p > pL_S) {
+            const double A = 2.0 / ((g + 1.0) * rhoL_S);
+            const double B = ((g - 1.0) / (g + 1.0)) * pL_S;
 
-            return (p_star - pL_S) * std::sqrt(A / (p_star + B));
+            const double denom = clamp_min(p + B, tol_S);
+            return (p - pL_S) * std::sqrt(safe_div(A, denom, tol_S));
         }
 
-        return (2.0 * cL_S) / (gammaL - 1.0)
-            * (std::pow(p_star / pL_S, (gammaL - 1.0) / (2.0 * gammaL)) - 1.0);
+        const double ratio = safe_div(p, pL_S, tol_S);
+
+        return (2.0 * cL_S) / (g - 1.0)
+            * (std::pow(ratio, (g - 1.0) / (2.0 * g)) - 1.0);
     }
 
 
-    // [1.4] Right wave function
     inline double fR(double p_star) const
     {
-        const double gammaR = paramsR_S.gamma;
+        const double p = clamp_min(p_star, tol_S);
+        const double g = paramsR_S.gamma;
 
-        if (p_star > pR_S) {
-            const double A = 2.0 / ((gammaR + 1.0) * rhoR_S);
-            const double B = ((gammaR - 1.0) / (gammaR + 1.0)) * pR_S;
+        if (p > pR_S) {
+            const double A = 2.0 / ((g + 1.0) * rhoR_S);
+            const double B = ((g - 1.0) / (g + 1.0)) * pR_S;
 
-            return (p_star - pR_S) * std::sqrt(A / (p_star + B));
+            const double denom = clamp_min(p + B, tol_S);
+            return (p - pR_S) * std::sqrt(safe_div(A, denom, tol_S));
         }
 
-        return (2.0 * cR_S) / (gammaR - 1.0)
-            * (std::pow(p_star / pR_S, (gammaR - 1.0) / (2.0 * gammaR)) - 1.0);
+        const double ratio = safe_div(p, pR_S, tol_S);
+
+        return (2.0 * cR_S) / (g - 1.0)
+            * (std::pow(ratio, (g - 1.0) / (2.0 * g)) - 1.0);
+    }
+
+
+    // derivatives 
+    inline double dfL(double p_star) const
+    {
+        const double p = clamp_min(p_star, tol_S);
+        const double g = paramsL_S.gamma;
+
+        if (p > pL_S) {
+            const double A = 2.0 / ((g + 1.0) * rhoL_S);
+            const double B = ((g - 1.0) / (g + 1.0)) * pL_S;
+
+            const double denom = clamp_min(p + B, tol_S);
+            const double sqrt_term = std::sqrt(safe_div(A, denom, tol_S));
+
+            return sqrt_term * (1.0 - 0.5 * safe_div(p - pL_S, denom, tol_S));
+        }
+
+        const double ratio = safe_div(p, pL_S, tol_S);
+
+        return safe_div(1.0, rhoL_S * cL_S, tol_S)
+            * std::pow(ratio, -(g + 1.0) / (2.0 * g));
+    }
+
+
+    inline double dfR(double p_star) const
+    {
+        const double p = clamp_min(p_star, tol_S);
+        const double g = paramsR_S.gamma;
+
+        if (p > pR_S) {
+            const double A = 2.0 / ((g + 1.0) * rhoR_S);
+            const double B = ((g - 1.0) / (g + 1.0)) * pR_S;
+
+            const double denom = clamp_min(p + B, tol_S);
+            const double sqrt_term = std::sqrt(safe_div(A, denom, tol_S));
+
+            return sqrt_term * (1.0 - 0.5 * safe_div(p - pR_S, denom, tol_S));
+        }
+
+        const double ratio = safe_div(p, pR_S, tol_S);
+
+        return safe_div(1.0, rhoR_S * cR_S, tol_S)
+            * std::pow(ratio, -(g + 1.0) / (2.0 * g));
+    }
+
+
+    // densities 
+    inline double rho_star_L(double p_star) const
+    {
+        const double p = clamp_min(p_star, tol_S);
+        const double g = paramsL_S.gamma;
+
+        if (p > pL_S) {
+            const double r = safe_div(p, pL_S, tol_S);
+
+            const double num = r + (g - 1.0) / (g + 1.0);
+            const double den = ((g - 1.0) / (g + 1.0)) * r + 1.0;
+
+            return rhoL_S * safe_div(num, den, tol_S);
+        }
+
+        return rhoL_S * std::pow(safe_div(p, pL_S, tol_S), 1.0 / g);
+    }
+
+
+    inline double rho_star_R(double p_star) const
+    {
+        const double p = clamp_min(p_star, tol_S);
+        const double g = paramsR_S.gamma;
+
+        if (p > pR_S) {
+            const double r = safe_div(p, pR_S, tol_S);
+
+            const double num = r + (g - 1.0) / (g + 1.0);
+            const double den = ((g - 1.0) / (g + 1.0)) * r + 1.0;
+
+            return rhoR_S * safe_div(num, den, tol_S);
+        }
+
+        return rhoR_S * std::pow(safe_div(p, pR_S, tol_S), 1.0 / g);
     }
 };
-
 
 
 
