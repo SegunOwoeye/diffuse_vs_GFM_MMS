5ad1d28bf7c35e8b42ff363dc7615336adc13713
SegunOwoeye <olusegunowoeye@icloud.com>
2026-04-22 12:52:10 +0100

    Update boundary.hpp
    Adapted Zero-gradient boundaries towards new system

--- src/dim/solver/advance/boundary.hpp
+++ src/dim/solver/advance/boundary.hpp
@@ -1,18 +1,19 @@
 #pragma once
 
-#include <vector>
 #include <array>
+#include <vector>
 
 #include "src/dim/state.hpp"
 
+namespace dim {
 
-// [0] Zero-gradient boundary (placeholder)
-template<int DIM, int NMAT>
-inline void apply_boundary_conditions(
-    std::vector<Conserved<DIM, NMAT>>& U,
-    const std::array<int, DIM>& N
-)
-{
-    // intentionally empty for now
-    // handled implicitly in flux via copying
-}
+    template<int DIM>
+    inline void apply_boundary_conditions(
+        std::vector<State<DIM>>&,
+        const std::array<int, DIM>&
+    )
+    {
+        // Zero-gradient boundaries are handled implicitly by the line solver.
+    }
+
+} 
