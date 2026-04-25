0e34b8ff043a6094f091fb72a03604fd8387019d
SegunOwoeye <olusegunowoeye@icloud.com>
2026-04-25 13:58:29 +0100

    Update _master_test_run.sh
    Now includes validation for single material euler solver and 1d GFM and DIM solvers

--- tests/_master_test_run.sh
+++ tests/_master_test_run.sh
@@ -7,7 +7,16 @@ chmod +x tests/basic_toro_tests.sh
 chmod +x tests/GFM_validation_tests.sh
 chmod +x tests/DIM_validation_tests.sh
 
-# [2] Run Files
+# [2] Clear CSV Output
+CSV_DIR="data/csv"
+
+if [ -d "$CSV_DIR" ]; then
+    find "$CSV_DIR" -mindepth 1 -maxdepth 1 -exec rm -rf {} +
+else
+    mkdir -p "$CSV_DIR"
+fi
+
+# [3] Run Files
 
 ./tests/basic_toro_tests.sh
 ./tests/GFM_validation_tests.sh
